eafc51146a2b904e3175665eb4106fa190af921e
KetyJomes <jomessofiaketlyn@gmail.com>
2025-08-28 09:09:50 -0300

    Quadrado Azul - tipo flecha

--- aula07/src/App.jsx
+++ aula07/src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Flecha } from './componentes/Flecha'
-import { Titulo } from './componentes/anonima'
+import { Blue } from './componentes/blue' /*Importando a função que esta no arquivo blue */
 
 function App() {
 
   return (
     <>  
-        <h1>Hello World! </h1> {/*Comum*/}
-        {<Flecha /> /*Flecha - Melhor componente*/}
-        <Titulo texto="Hello World" /> {/*Anonima*/}
+        <Blue/> {/*Tipo Flecha*/}
     </>
   )
 }
---
+++ aula07/src/componentes/blue.jsx
@@ -0,0 +1,9 @@
+import'./blue.css'
+
+export const Blue = () => {
+  return(
+    <> {/* Esse fragmento é para retornar mais de uma div*/}
+      <div className='BlueSquare'></div>
+    </>
+  )
+}

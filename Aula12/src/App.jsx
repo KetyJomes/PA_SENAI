4436b44e4c21b9a4036e5f379b03f7618e5ad204
KetyJomes <jomessofiaketlyn@gmail.com>
2025-09-17 08:32:51 -0300

    Aula 12 - Roteamento

---
+++ Aula12/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path="/about" element={<About />} />      
+      </Routes>  
+    </BrowserRouter>
+  )
+
+}
+
+export default App

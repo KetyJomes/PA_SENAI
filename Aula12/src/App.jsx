354ea8427571bf6bd03ef424f53d7a650bddbf06
KetyJomes <jomessofiaketlyn@gmail.com>
2025-09-17 09:08:56 -0300

    Definindo rotas dinâmicas (Não dei crtl s de novo)

--- Aula12/src/App.jsx
+++ Aula12/src/App.jsx
@@ -16,6 +16,8 @@ const App = () => {
         <Route path='/' element = {<Home />} />
         {/* Ao acessar o localHost, se quiser que apareça o que tem dentro do componente about, colocar /about do lado do localHost */}
         <Route path="/about" element={<About />} />  
+        {/* Esse é o caminho da URL para a rota. O :nome é uma parâmetro de rota dinâmico. 
+        Ou seja, ele pode ser substituído por qualquer valor quando o usuário acessar a URL. */}
         <Route path="/profile/:nome" element={<Profile />} />  
 
       </Routes>  

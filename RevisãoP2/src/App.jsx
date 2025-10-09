d873c2fee475df460c626127ec202803cd16b60b
KetyJomes <jomessofiaketlyngmail.com>
2025-10-09 11:39:12 -0300

    Continuação da revisão - UseState, UseEffect e Props

--- RevisãoP2/src/App.jsx
+++ RevisãoP2/src/App.jsx
@@ -1,33 +1,118 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// function App() { USESTATE
+//   const [value, setValue] = useState(0)
+//   const [text, setText] = useState()
+//   const [show, setShow] = useState(true)
+
+//   return (
+//     <>
+//       <button onClick={() => setValue(value + 1)}>+</button>
+//       {value}
+//       <button onClick={() => setValue(value - 1)}>-</button>
+
+//       <br />
+
+//       <input type="text" onChange={(e) => setText(e.target.value)} />
+//       <h1>{text}</h1>
+
+//       <br />
+
+//       <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
+//       {show &&
+//       <div style={{backgroundColor: 'blueviolet', width: '200px', height: '200px'}}></div>}
+
+//     </>
+//   )
+// }
+
+// export default App
+
+{/* <> </> é um fragmento usado para retornar mais de um valor  */}
+
+
+// function App() { USEEFFECT
+//   const [value, setValue] = useState(0)
+//   const [text, setText] = useState()
+//   const [show, setShow] = useState(true)
+
+//   useEffect(() => {
+//     console.log('Testando o Text')
+//   },[text])
+
+//    useEffect(() => {
+//     console.log('Testando o Value')
+//   },[value])
+
+//   return (
+//     <>
+//       <button onClick={() => setValue(value + 1)}>+</button>
+//       {value}
+//       <button onClick={() => setValue(value - 1)}>-</button>
+
+//       <br />
+
+//       <input type="text" onChange={(e) => setText(e.target.value)} />
+//       <h1>{text}</h1>
+
+//       <br />
+
+//       <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
+//       {show &&
+//       <div style={{backgroundColor: 'blueviolet', width: '200px', height: '200px'}}></div>}
+
+//     </>
+//   )
+// }
+
+// export default App
+
+function Pessoa({nome, sobrenome, idade}){
+  return(
+    <>
+    <h1>Nome: {nome} </h1>
+    <h1>Sobrenome: {sobrenome}</h1>
+    <h1>Idade: {idade}</h1>
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(0)
+  const [text, setText] = useState()
+  const [show, setShow] = useState(true)
+
+  useEffect(() => {
+    console.log('Testando o Text')
+  },[text])
+
+   useEffect(() => {
+    console.log('Testando o Value')
+  },[value])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setValue(value + 1)}>+</button>
+      {value}
+      <button onClick={() => setValue(value - 1)}>-</button>
+
+      <br />
+
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <h1>{text}</h1>
+
+      <br />
+
+      <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
+      {show &&
+      <div style={{backgroundColor: 'blueviolet', width: '200px', height: '200px'}}></div>}
+
+      <Pessoa nome='Ketlyn' sobrenome='Jomes' idade="20"/>
+      <Pessoa nome='Maria' sobrenome='Jomes' idade="13"/>
+
     </>
   )
 }

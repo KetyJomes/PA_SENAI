d8e265cf9874ac85ff7d64d0298a6d9cd5fc2530
KetyJomes <jomessofiaketlyn@gmail.com>
2025-10-16 11:35:41 -0300

    Props revisão

--- RevisãoP2/src/App.jsx
+++ RevisãoP2/src/App.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Botao}  from './components/Botao'
 
-// function App() { USESTATE
+// function App() {                                  USESTATE
 //   const [value, setValue] = useState(0)
 //   const [text, setText] = useState()
 //   const [show, setShow] = useState(true)
@@ -34,7 +35,9 @@ import './App.css'
 {/* <> </> é um fragmento usado para retornar mais de um valor  */}
 
 
-// function App() { USEEFFECT
+
+
+// function App() {                                      USEEFFECT
 //   const [value, setValue] = useState(0)
 //   const [text, setText] = useState()
 //   const [show, setShow] = useState(true)
@@ -70,51 +73,72 @@ import './App.css'
 
 // export default App
 
-function Pessoa({nome, sobrenome, idade}){
-  return(
-    <>
-    <h1>Nome: {nome} </h1>
-    <h1>Sobrenome: {sobrenome}</h1>
-    <h1>Idade: {idade}</h1>
-    </>
-  )
-}
 
-function App() {
-  const [value, setValue] = useState(0)
-  const [text, setText] = useState()
-  const [show, setShow] = useState(true)
 
-  useEffect(() => {
-    console.log('Testando o Text')
-  },[text])
 
-   useEffect(() => {
-    console.log('Testando o Value')
-  },[value])
+// function Pessoa({nome, sobrenome, idade}){              PROPS
+//   return(
+//     <>
+//     <h1>Nome: {nome} </h1>
+//     <h1>Sobrenome: {sobrenome}</h1>
+//     <h1>Idade: {idade}</h1>
+//     </>
+//   )
+// }
 
-  return (
-    <>
-      <button onClick={() => setValue(value + 1)}>+</button>
-      {value}
-      <button onClick={() => setValue(value - 1)}>-</button>
+// function App() {
+//   const [value, setValue] = useState(0)
+//   const [text, setText] = useState()
+//   const [show, setShow] = useState(true)
 
-      <br />
+//   useEffect(() => {
+//     console.log('Testando o Text')
+//   },[text])
 
-      <input type="text" onChange={(e) => setText(e.target.value)} />
-      <h1>{text}</h1>
+//    useEffect(() => {
+//     console.log('Testando o Value')
+//   },[value])
 
-      <br />
+//   return (
+//     <>
+//       <button onClick={() => setValue(value + 1)}>+</button>
+//       {value}
+//       <button onClick={() => setValue(value - 1)}>-</button>
 
-      <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
-      {show &&
-      <div style={{backgroundColor: 'blueviolet', width: '200px', height: '200px'}}></div>}
+//       <br />
 
-      <Pessoa nome='Ketlyn' sobrenome='Jomes' idade="20"/>
-      <Pessoa nome='Maria' sobrenome='Jomes' idade="13"/>
+//       <input type="text" onChange={(e) => setText(e.target.value)} />
+//       <h1>{text}</h1>
 
-    </>
-  )
-}
+//       <br />
 
-export default App
+//       <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
+//       {show &&
+//       <div style={{backgroundColor: 'blueviolet', width: '200px', height: '200px'}}></div>}
+
+//       <Pessoa nome='Ketlyn' sobrenome='Jomes' idade="20"/>
+//       <Pessoa nome='Maria' sobrenome='Jomes' idade="13"/>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// function App() {                                      PROPS/2
+
+//   return (
+//     <>
+
+//       <Botao cor='purple' conteudo="Login" altura="50px" largura="200px"></Botao>
+//       <br />
+//       <br />
+//       <Botao cor='gray' conteudo="Cadastro" altura="50px" largura="100px"></Botao>
+
+//     </>
+//   )
+// }
+
+// export default App
---
+++ RevisãoP2/src/components/Botao.jsx
@@ -0,0 +1,9 @@
+export function Botao({cor, conteudo, altura, largura}){
+  return(
+    <>
+    <button style={{backgroundColor: cor, height: altura, width: largura}}>{conteudo}</button>
+    </>
+  )
+}
+
+export default Botao

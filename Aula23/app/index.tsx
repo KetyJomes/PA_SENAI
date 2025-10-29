a1b71389e0f891956bd4976b731189b820381a17
KetyJomes <jomessofiaketlyn@gmail.com>
2025-10-29 11:33:05 -0300

    React Native

---
+++ Aula23/app/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+  <View>
+    <Text style={style.colorBlue}>Hello World!!!</Text>
+    <br />
+    <View style={style.square}></View>
+   
+  </View>
+  );
+}
+
+const style = StyleSheet.create({
+  colorBlue: {
+    color: "blue",
+    fontSize: 50
+  },
+  square:{
+    height: 301,
+    width: 301,
+    backgroundColor: 'rebeccapurple'
+  }
+})
+
+
